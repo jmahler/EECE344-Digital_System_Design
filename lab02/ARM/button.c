08f6f51311f2c5430c67710325064da89213c3a6
Jeremiah Mahler <jmmahler@gmail.com>
2012-04-03 09:55:30 -0700

    moved button documentation to .h from .c

--- lab02/ARM/button.c
+++ lab02/ARM/button.c
@@ -1,24 +1,5 @@
 #include "button.h"
 
-/* 
- * enable_button(), button_pressed()
- *
- * The STM32L Discovery contains a USER button.
- * This functions enables it so it can be tested
- * to see if it is pressed or not.
- *
- * SYNOPSIS
- * --------
- *
- *  enable_button();
- *
- *	if (button_pressed()) {
- *		// do something
- *	} else {
- *		// do something else
- * 	}
- */
-
 void enable_button()
 {
 	int i = 0;
--- lab02/ARM/button.h
+++ lab02/ARM/button.h
@@ -1,5 +1,24 @@
 #include "stm32l1xx.h"
 
+/* 
+ * enable_button(), button_pressed()
+ *
+ * The STM32L Discovery contains a USER button.
+ * This functions enables it so it can be tested
+ * to see if it is pressed or not.
+ *
+ * SYNOPSIS
+ * --------
+ *
+ *  enable_button();
+ *
+ *	if (button_pressed()) {
+ *		// do something
+ *	} else {
+ *		// do something else
+ * 	}
+ */
+
 void enable_button();
 
 unsigned int button_pressed();
